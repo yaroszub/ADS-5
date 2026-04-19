788295457a1cb65367083e9a4af3dcddbe4498af
yaroszub <yaroslav.zubkov.2020@bk.ru>
2026-04-19 21:05:24 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -4,7 +4,39 @@
 
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+ private:
+  T arr[size];
+  int top_index;
+
+ public:
+  TStack() : top_index(-1) {}
+
+  void push(T value) {
+    if (top_index < size - 1) {
+      arr[++top_index] = value;
+    }
+  }
+
+  void pop() {
+    if (top_index >= 0) {
+      --top_index;
+    }
+  }
+
+  T get() const {
+    if (top_index >= 0) {
+      return arr[top_index];
+    }
+    return T();
+  }
+
+  bool isempty() const {
+    return top_index == -1;
+  }
+
+  int stack_size() const {
+    return top_index + 1;
+  }
 };
 
 #endif  // INCLUDE_TSTACK_H_
