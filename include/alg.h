7b2da7d1ad629aab679f5784b7600eb31c98e0d8
yaroszub <yaroslav.zubkov.2020@bk.ru>
2026-04-19 20:42:30 +0300

    Update alg.h

--- include/alg.h
+++ include/alg.h
@@ -2,9 +2,41 @@
 #ifndef INCLUDE_ALG_H_
 #define INCLUDE_ALG_H_
 
-#include <string>
+template<typename T, int MaxSize>
+class TStack {
+ private:
+  T arr[MaxSize];
+  int top_index;
 
-std::string infx2pstfx(std::string);
-int eval(std::string);
+ public:
+  TStack() : top_index(-1) {}
 
-#endif  // INCLUDE_ALG_H_
+  void push(T value) {
+    if (top_index < MaxSize - 1) {
+      arr[++top_index] = value;
+    }
+  }
+
+  void pop() {
+    if (top_index >= 0) {
+      --top_index;
+    }
+  }
+
+  T get() const {
+    if (top_index >= 0) {
+      return arr[top_index];
+    }
+    return T();
+  }
+
+  bool isempty() const {
+    return top_index == -1;
+  }
+
+  int stack_size() const {
+    return top_index + 1;
+  }
+};
+
+#endif  // INCLUDE_TSTACK_H_
